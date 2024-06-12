98770934054a7c019a1060507994e5e6d684acd9
mad <mad@yahoo.com>
2024-06-12 11:12:46 +0330

    setup with tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import CardContent from "./Components/CardContent";
+function App() {
+  return (
+    <div className="App">
+      <CardContent />
+    </div>
+  );
+}
+
+export default App;
